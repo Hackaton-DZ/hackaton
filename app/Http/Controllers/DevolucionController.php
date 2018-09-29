41403682ffe7c47888026aa8efec4d34464d88ed
David Chacca <davidchacca@gmail.com>
2018-09-29 04:44:02 -0500

    table productos

--- app/Http/Controllers/DevolucionController.php
+++ app/Http/Controllers/DevolucionController.php
@@ -21,7 +21,7 @@ class DevolucionController extends Controller
     }
 
     public function  create(){
-        $ventas= json_decode(file_get_contents("https://inretail-hackathon-api.azurewebsites.net/api/promart/Ventas"));
+        /*$ventas= json_decode(file_get_contents("https://inretail-hackathon-api.azurewebsites.net/api/promart/Ventas"));
         //$Plan= new Venta();
         foreach($ventas as $venta){
             $Plan= new Venta();
@@ -33,13 +33,14 @@ class DevolucionController extends Controller
             $Plan= new Cliente();
             $Plan->fill((array)$venta );
             $Plan->save();
-        }
+        }*/
        // $clientes=$ventas->pluck('nombreCLiente','documento')->prepend('Seleccione una Opcion', '');
-        $clientes = Cliente::get()->pluck('nombre', 'id')->prepend('Seleccione una Opcion', '');
-        $venta = Venta::all();
+        
+       $clientes = Venta::where('')->pluck('nombreCliente', 'apellidos','documento');
+        //$venta = Venta::all();
         
         //return view('devoluciones.create', compact('clientes','name'));
-        return $venta;
+        return $clientes;
     }
     
 
