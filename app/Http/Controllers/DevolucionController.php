829167e47d0a97afb7f5d640a9a99a13dcf0be1a
David Chacca <davidchacca@gmail.com>
2018-09-28 23:27:24 -0500

    -master

--- app/Http/Controllers/DevolucionController.php
+++ app/Http/Controllers/DevolucionController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class DevolucionController extends Controller
 {
     public function  index(){
-        return view('devolucion.index');
+        return view('devoluciones.index');
     }
 }
--- resources/views/devoluciones/index.blade.php
+++ resources/views/devoluciones/index.blade.php
@@ -1,1 +1,11 @@
-hola este es devoluviones index
+@extends('layouts.app')
+@section('content')
+<div class="card">
+    <img class="card-img-top" src="holder.js/100x180/" alt="">
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Text</p>
+    </div>
+</div>
+Holacontenido index devolucones
+@endsection
