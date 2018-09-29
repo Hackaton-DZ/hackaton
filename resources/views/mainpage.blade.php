711c9f5bc0f0e9eb1abed14eda848a4ea23e9d94
Christian <30902930+christiancazu@users.noreply.github.com>
2018-09-28 22:34:56 -0500

    ASDA
    ASDA
    
    Co-Authored-By: Christian <christiancazu@gmail.com>

---
+++ resources/views/mainpage.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="<?= IMG?>logo/logo-icon.png">
+        <title><?= $title ?></title>
+        <link href="/css/bootstrap.css" rel="stylesheet" media="screen">        
+        <link href="/css/font-awesome.css" rel="stylesheet" media="screen">   
+        <link href="/css/styles.css" rel="stylesheet" media="screen">           
+    </head>
+<body>
+
+    <h1>qwWEEew</h1>
+
+<script src="/js/jquery.js"></script>
+<script src="/js/bootstrap.js"></script>
+<script src="/js/script.js"></script>
+</body>
+</html>
